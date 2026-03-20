1a13bdb7537869a632d37b461e4dc6bebf83eadb
jean bueno <149797874+Kanuro@users.noreply.github.com>
2026-03-20 11:05:49 -0300

    Update churras.c

--- churras.c
+++ churras.c
@@ -8,9 +8,9 @@
 int main()
 {
     int homem, mulher, kid;
-    float frango;
+    float frango, picanha,queijo, costela;
     
-       frango= 1.5;
+       
    
 
     printf("\n ---quantos homems?---  ");
@@ -18,21 +18,30 @@ int main()
 
     printf("\n ---quantas mulheres?--- ");
     scanf("%i", &mulher);
+    
     printf("\n ---quantas criancas?--- ");
     scanf("%i", &kid);
 
-      frango = (homem * 800) + (mulher * 600) + (kid * 300);
+          frango = (homem * 800) + (mulher * 600) + (kid * 300);
       frango = frango/1000;
-    
-     picanha = (homem * 900) + (mulher * 750) + (kid * 500);
+
+     printf("quantidade de frango: %.2f,kg", frango);
+     
+      picanha = (homem * 900) + (mulher * 750) + (kid * 500);
       picanha = picanha/1000;
 
-     printf("\nquantidade de picanha: %.2f,kg", picanha);
+     printf("n\nquantidade de picanha: %.2f,kg", picanha);
      
       queijo = (homem * 500) + (mulher * 300) + (kid * 400);
       queijo = queijo/1000;
 
-     printf("quantidade de frango: %.2f,kg", frango);
+     printf("n\nquantidade de queijo: %.2f,kg", queijo);
+     
+       costela = (homem * 800) + (mulher * 600) + (kid * 400);
+       costela = costela/1000;
+
+     printf("n\nquantidade de costela: %.2f,kg", costela);
+     
 
 
 
